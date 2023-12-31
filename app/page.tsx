e7c376c21c0b7eeeddc8c8d62091cdeb17dab772
c0de_l0rd <tyrannicaloverlord99@gmail.com>
2024-01-01 00:32:12 +0200

    add new note button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import Notes from '@/components/notes'
+import AddNote from '@/components/addNote'
 
 export default function Home() {
   return (
     <>
+    <AddNote/>
     <Notes/>
     <Notes/>
     <Notes/>
---
+++ components/addNote.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { MdOutlineEdit } from "react-icons/md";
+
+
+function AddNote() {
+  return (
+    <div className='bg-gray-400 bg-opacity-50 w-[70px] h-[70px] rounded-[25px] pt-4 fixed ml-[76%] mt-[123%]'>
+        <MdOutlineEdit size={42} className='m-auto '/>
+    </div>
+  )
+}
+
+export default AddNote
